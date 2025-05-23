85b142eb3de054d95b7c84210176579bb1bc0813
Victor Paro <victorbparo@gmail.com>
2025-05-23 01:24:16 -0300

    Criando componentes e funções de teste para requisições à API

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,33 @@
 import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { Platform, StyleSheet, Button } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import axios from 'axios';
+
+const apiUrl = process.env.EXPO_PUBLIC_API_URL_TEST;
+
+const buttonTest = () => {
+  console.log('test');
+}
+const getAllUsers = async () => {
+  console.log('URL DEBUG ', apiUrl);
+  /*try {
+    const response = await axios.get(apiUrl!);
+    console.log(response);
+  } catch (err) {
+    console.log('Erro: ', err);
+  }*/
+  const response = axios.get(apiUrl!)
+  .then(response =>{
+    console.log('Resposta: ', response);
+  })
+  .catch(err => {
+    console.log('Erro: ', err);
+  });
+
+}
 
 export default function HomeScreen() {
   return (
@@ -17,39 +40,15 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
+        <ThemedText type="title">Bem-vindo ao Parkingless</ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
+        <ThemedText type="subtitle">Botão de teste</ThemedText>
+        <Button title="Teste" accessibilityLabel="Botão de teste" onPress={buttonTest}/>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
+        <ThemedText type="subtitle">Imprimir lista de usuários</ThemedText>
+        <Button title="Pressione aqui" accessibilityLabel="Botão lista de usuários" onPress={getAllUsers}/>
       </ThemedView>
     </ParallaxScrollView>
   );
